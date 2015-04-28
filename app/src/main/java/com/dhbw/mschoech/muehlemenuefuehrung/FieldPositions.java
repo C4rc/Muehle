7a43bfd5be6bf269d6d446ac3def94db3c735c75
C4rc <it13164@lba.ba>
2015-04-28 19:12:47 +0200

    2015-04-28

---
+++ app/src/main/java/com/dhbw/mschoech/muehlemenuefuehrung/FieldPositions.java
@@ -0,0 +1,13 @@
+package com.dhbw.mschoech.muehlemenuefuehrung;
+
+/**
+ * Created by mschoech on 28.04.2015.
+ */
+public abstract class FieldPositions {
+    float positions[][][];
+
+    public float[] compare(int x, int y){
+        return null;
+    }
+    public void field(){}
+}
